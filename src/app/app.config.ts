6a4ebf0ecb30c9d003b75231b2edc074b1352419
Salwa Faraj <167221572+salwafaraj@users.noreply.github.com>
2024-12-30 15:39:19 +0100

    Delete src directory

--- src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-
-export const appConfig = {
-  providers: [
-    provideRouter(appRoutes)
-  ]
-};
--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-
-export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
-];
